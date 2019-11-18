5fec1673625d18c544ce3e64f0e733a9e7a1b989
JordanG <26260@ma-web.nl>
2019-11-18 12:42:45 +0100

    Basic movement added

--- Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Physics
     private Rigidbody rb;
+    private Vector3 inputVector;
+    [SerializeField] private float speed = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+    }    
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
+        rb.velocity = inputVector;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
     }
 }
