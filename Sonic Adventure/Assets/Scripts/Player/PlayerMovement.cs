360cdd3586dcf93fcf4bde101894dcd837c6c90a
JordanG <26260@ma-web.nl>
2019-11-18 15:15:36 +0100

    Slope rotation

--- Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 inputVector;
     [SerializeField] private float speed = 3;
 
+    private Vector3 upright;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +27,11 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, 0.5f, -(transform.up), out hit, 1.1f))
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));
+        }
     }
 }
