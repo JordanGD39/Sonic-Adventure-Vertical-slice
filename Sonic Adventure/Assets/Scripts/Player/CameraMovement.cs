1e9781e306f411b866670a59b3a62df2b5a24a00
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-20 00:34:16 +0100

    Changed Camera And Movement

---
+++ Sonic Adventure/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _sonic;
+
+    //Camera position
+    private Transform transform;
+
+    //Camera and player position difference
+    private Vector3 difPos;
+    
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        difPos = new Vector3(0.0f, 2.75f, -7.0f);
+        transform.position = _sonic.transform.position + difPos;
+    }
+}
--- Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    const float speedAccel = 1.0f;
+
+    //Speed
+    [SerializeField]
+    private float _speed;
+
     //Physics
     private Rigidbody rb;
     private Vector3 movement;
-    [SerializeField] private float speed = 3;
+    private Quaternion rotationMov;
 
     private Vector3 upright;
 
@@ -28,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
         if (Physics.SphereCast(transform.position, 0.5f, -transform.up, out hit, 1.1f))
         {
             Quaternion quat = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));
-            Debug.Log(hit.normal);
+            //Debug.Log(hit.normal);
 
             transform.rotation = new Quaternion(quat.x, transform.rotation.y, transform.rotation.z, quat.w);
         }
@@ -40,15 +46,33 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        float step = (_speed + checkInput(speedAccel, speedAccel)) * Time.deltaTime;
+
         if (transform.rotation.x <= 0.6f && transform.rotation.x >= -0.6f)
         {
-            rb.AddRelativeForce(movement * speed);
+            //rb.AddRelativeForce(movement * _speed); //Changing position on ice
+            transform.position += (movement * step);  //Changing position without slipping
             rb.useGravity = true;
         }
         else
         {
-            rb.AddRelativeForce(movement * (speed * 0.85f));
+            rb.AddRelativeForce(movement * (_speed * 0.85f));
+            //transform.position += (movement * 0.85f * step);
             rb.useGravity = false;
         }              
+    }
+
+    private float checkInput(float speedAccel, float accelBeginValue)
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            speedAccel *= speedAccel * 1.01f;
+        }
+        else
+        {
+            speedAccel = accelBeginValue;
+        }
+
+        return speedAccel;
     }
 }
