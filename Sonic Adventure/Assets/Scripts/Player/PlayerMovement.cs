e2d5c26869b3bad89a29e499f6eebe65c85f8d86
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-20 15:16:28 +0100

    Camera is not yet fully fixed

--- Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
     //Physics
     private Rigidbody rb;
     private Vector3 movement;
+    private Vector3 movementForce;
     [SerializeField] private float speed = 3;
+    [SerializeField] private float wallSpeed = 3;
+    private bool loopTime = false;
 
     private Vector3 upright;
 
@@ -22,40 +25,48 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        movement = new Vector3(Camera.main.transform.right.x * (moveHorizontal * speed), 0, Camera.main.transform.forward.z * (moveVertical * speed));
+        movement = new Vector3(moveHorizontal * speed, rb.velocity.y, moveVertical * speed);
+        movementForce = new Vector3(moveHorizontal, 0, moveVertical);
 
-        transform.LookAt(transform.position + new Vector3(movement.x, 0, movement.z));
+        Vector3 tempVect = transform.position + new Vector3(movement.x, 0, movement.z);
+        transform.LookAt(new Vector3(tempVect.x, transform.position.y, tempVect.z));
 
         RaycastHit hit;
         if (Physics.SphereCast(transform.position, 0.5f, -transform.up, out hit, 1.1f))
         {
             Quaternion quat = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));
-            Debug.Log(hit.normal);
 
             transform.rotation = new Quaternion(quat.x, transform.rotation.y, transform.rotation.z, quat.w);
         }
-        //else
-        //{
-        //    transform.rotation = Quaternion.Euler(0, transform.rotation.y, transform.rotation.z);
-        //}
+        else
+        {
+            transform.rotation = new Quaternion(0, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            speed -= 0.6f;
+            if (speed < 7)
+            {
+                speed += 0.6f;
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
-        
-        if (rb.velocity.magnitude > 2.6f && rb.velocity.magnitude <= 16f)
+        if (rb.velocity.magnitude > 2.6f && rb.velocity.magnitude <= 14f)
+        {
+            if (speed < 16)
+            {
+                speed += 0.5f;
+            }
+        }
+        else if (rb.velocity.magnitude > 14f)
         {
             if (speed < 18)
             {
                 speed += 0.5f;
-            }                   
-        }
-        else if (rb.velocity.magnitude > 16f)
-        {
-            if (speed < 20)
+            }
+            else
             {
-                speed += 0.5f;
+                speed = 18;
             }
         }
         else if (rb.velocity.magnitude <= 2.6f)
@@ -70,14 +81,29 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (transform.rotation.x <= 0.6f && transform.rotation.x >= -0.6f)
-        {            
-            rb.useGravity = true;
+        if (transform.rotation.x <= 0.3f && transform.rotation.x >= -0.3f)
+        {
+            rb.velocity = Camera.main.transform.forward + movement;
+            loopTime = false;
         }
         else
         {
-            
-            rb.useGravity = false;
-        }              
+            if (!loopTime)
+            {
+                wallSpeed = speed;
+            }
+            loopTime = true;
+            if (transform.rotation.y > 0)
+            {
+                transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w);
+            }
+            else if (transform.rotation.y < 0)
+            {
+                transform.rotation = new Quaternion(transform.rotation.x, -180, transform.rotation.z, transform.rotation.w);
+            }
+            wallSpeed *= 0.995f;
+            rb.AddRelativeForce(movementForce * wallSpeed);
+            rb.AddForce(Physics.gravity * 0.2f * rb.mass);
+        }
     }
 }
--- Sonic Adventure/Assets/Scripts/ThirdPersonCameraControl.cs
+++ Sonic Adventure/Assets/Scripts/ThirdPersonCameraControl.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class ThirdPersonCameraControl : MonoBehaviour
 {
-    float rotationSpeed = 1;
-    public Transform target;
+    const float rotationSpeed = 30.0f;
 
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _smoothSpeed = 0.125f;
+
+    [SerializeField]
+    private Vector3 _offset;
+
+    private float turnXAxis;
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-    }    
+
+        turnXAxis = 0.0f;
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Mouse X");
+
+        if (turnXAxis > _offset.z || turnXAxis < -_offset.z)
+        {
+            turnXAxis -= moveHorizontal;
+        }
+        else
+        {
+            //turnXAxis -= moveHorizontal;
+        }
+    }
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //Vector3 desiredPosition = _target.position + _offset;
+        Vector3 desiredPosition = _target.position + new Vector3(turnXAxis, _offset.y, _offset.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
         transform.position = smoothedPosition;
 
-        transform.LookAt(target);
+        //transform.Translate(new Vector3(-moveHorizontal, 0.0f, 0.0f) * 25.0f * Time.deltaTime);
+        //transform.Translate(Vector3.right * 25.0f * Time.deltaTime);
+
+        transform.LookAt(_target);
     }
 }
