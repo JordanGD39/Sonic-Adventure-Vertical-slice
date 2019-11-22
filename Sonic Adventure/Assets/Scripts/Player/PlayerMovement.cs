cf8e16945898cc1ed3a974c91350d65d72b31c49
JordanG <26260@ma-web.nl>
2019-11-22 12:41:42 +0100

    Rotation changes

--- Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,13 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     //Physics
     private Rigidbody rb;
+    
     private Vector3 movement;
     [SerializeField] private float speed = 3;
     [SerializeField] private bool loopTime = false;
     [SerializeField] private bool offTheRamp = false;
     [SerializeField] private bool grounded = false;
-
-    private Vector3 upright;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +26,29 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         movement = new Vector3(moveHorizontal, 0, moveVertical);
 
-        //Vector3 tempVect = transform.position + Camera.main.transform.TransformVector(movement);
-        //transform.LookAt(new Vector3(tempVect.x, transform.position.y, tempVect.z));
+        if (!loopTime)
+        {
+            Vector3 tempVect = transform.position + Camera.main.transform.TransformVector(movement);
+            Quaternion rot;
+            rot = transform.rotation;
+            transform.LookAt(tempVect);
+            transform.localRotation = new Quaternion(rot.x, transform.localRotation.y, 0, transform.localRotation.w);
+        }
+        else
+        {
+            Vector3 tempVect = transform.position + transform.TransformVector(movement);
+            Quaternion rot;
+            rot = transform.rotation;
+            transform.LookAt(tempVect);
+            transform.localRotation = new Quaternion(rot.x, transform.localRotation.y, 0, transform.localRotation.w);
+        }
 
         RaycastHit hit;
         if (Physics.SphereCast(transform.position, 0.3f, -transform.up, out hit, 1.1f))
         {
-            Quaternion quat = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));            
-
-            transform.rotation = new Quaternion(quat.x, transform.rotation.y, transform.rotation.z, quat.w);
+            Quaternion quat = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));
+            Debug.Log(hit.normal);
+            transform.localRotation = new Quaternion(quat.x, transform.localRotation.y, transform.localRotation.z, transform.localRotation.w);
             grounded = true;
         }
         else
@@ -102,9 +115,7 @@ public class PlayerMovement : MonoBehaviour
                 loopTime = true;
                 rb.useGravity = false;
                 Vector3 tempVect = transform.TransformVector(movement);
-                Debug.Log(tempVect);
                 tempVect *= speed * 0.5f;
-                Debug.Log("after speed: " + tempVect);
                 rb.velocity = tempVect;
             }
 
@@ -117,3 +128,29 @@ public class PlayerMovement : MonoBehaviour
         }              
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
