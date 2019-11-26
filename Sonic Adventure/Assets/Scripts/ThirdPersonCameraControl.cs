697ef6fec024e11bfec3ab945f9378d4023f5661
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-26 22:28:00 +0100

    Camera Stops at Object

--- Sonic Adventure/Assets/Scripts/ThirdPersonCameraControl.cs
+++ Sonic Adventure/Assets/Scripts/ThirdPersonCameraControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ThirdPersonCameraControl : MonoBehaviour
 {
-    const string playerTag = "Player";
+    const string PLAYER_TAG = "Player";
 
     [SerializeField]
     private Transform _target;
@@ -14,6 +14,8 @@ public class ThirdPersonCameraControl : MonoBehaviour
 
     [SerializeField]
     private Vector3 _offset;
+
+    private float colliderRadius;
 
     private RaycastHit[] hit = new RaycastHit[2];
     private float rotationSpeed = 3.0f;
@@ -27,6 +29,8 @@ public class ThirdPersonCameraControl : MonoBehaviour
 
         transform.position = _target.position + _offset;
         playerCameraDistance = Vector3.Distance(hit[0].point, transform.position);
+
+        colliderRadius = (_offset.magnitude * 0.25f);
     }
 
     void FixedUpdate()
@@ -36,52 +40,26 @@ public class ThirdPersonCameraControl : MonoBehaviour
         _offset = Quaternion.AngleAxis(turnHorizontal * rotationSpeed, Vector3.up) * _offset;
         //transform.position = _target.position + _offset;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit[0]))
-        {
-            Debug.DrawRay(transform.position, transform.forward * hit[0].distance, Color.blue);
-            //Debug.Log("Front Ray hits " + hit[0].collider.tag);
+        Collider[] cameraCollision = Physics.OverlapSphere(transform.position, colliderRadius);
 
-            /*if (hit[0].collider.gameObject.tag != playerTag)
-            {
-                transform.position = _target.position + (transform.forward * hit[0].distance);
-            }*/
+        if (cameraCollision.Length <= 0)
+        {
+            transform.position = _target.position + _offset;
         }
+        else if (Vector3.Distance(_target.position, transform.position) > _offset.magnitude)
+        {
+            transform.position = _target.position + _offset;
+        }
+        else
+        {
 
-        if (Physics.Raycast(transform.position, -transform.forward, out hit[1]))
-        {
-            Debug.DrawRay(transform.position, -transform.forward * hit[1].distance, Color.red);
-            //Debug.Log("Back Ray hits " + hit[1].collider.tag);
-        }
-
-        if (hit[0].collider != null && hit[1].collider == null)
-        {
-            if (hit[0].collider.gameObject.tag == playerTag)
-            {
-                transform.position = _target.position + _offset;
-            }
-            else if (hit[0].distance < playerCameraDistance)
-            {
-                transform.position += (transform.forward * hit[0].distance);
-            }
-        }
-        else if (hit[0].collider != null && hit[1].collider != null)
-        {
-            if (hit[0].collider.gameObject.tag == playerTag && hit[1].distance > 0.05f)
-            {
-                transform.position = _target.position + _offset;
-                Debug.Log(playerCameraDistance);
-            }
-            else if (hit[0].distance < playerCameraDistance)
-            {
-                transform.position += (transform.forward * hit[1].distance);
-            }
-        }
-
-        if (hit[0].collider == null)
-        {
-            Debug.Log("No object visible.");
         }
 
         transform.LookAt(_target);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, colliderRadius);
     }
 }
