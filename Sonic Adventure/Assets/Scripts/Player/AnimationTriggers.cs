027cac66d40a8c0f5dd9fdd075d82594da062325
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-12-09 01:17:22 +0100

    Orca Animation Added

---
+++ Sonic Adventure/Assets/Scripts/Player/AnimationTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggers : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _triggers;
+
+    [SerializeField]
+    private Animator _orcaAnimator;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == _triggers[0].name)
+        {
+            //Debug.Log("Triggered");
+            _orcaAnimator.SetTrigger("Player Arrival");
+        }
+    }
+}
