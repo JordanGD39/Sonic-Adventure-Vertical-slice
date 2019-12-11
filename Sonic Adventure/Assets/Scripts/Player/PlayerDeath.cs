914ae451ad89c470f7156cdf8180f9bd1865b077
JordanG <26260@ma-web.nl>
2019-12-11 16:13:02 +0100

    Death anim added

--- Sonic Adventure/Assets/Scripts/Player/PlayerDeath.cs
+++ Sonic Adventure/Assets/Scripts/Player/PlayerDeath.cs
@@ -7,6 +7,7 @@ public class PlayerDeath : MonoBehaviour
 {
     private GameObject fadeObjectOut;
     private GameObject fadeObjectIn;
+    private Animator anim;
 
     private void Start()
     {
@@ -15,14 +16,17 @@ public class PlayerDeath : MonoBehaviour
         fadeObjectIn.SetActive(true);
         fadeObjectOut.SetActive(false);
         GameManager.instance.Dying = false;
+        anim = transform.GetChild(0).GetChild(0).GetComponent<Animator>();
+        anim.SetBool("Dead", false);
     }
 
     public IEnumerator Die()
     {
+        anim.SetBool("Dead", true);
         Camera.main.GetComponent<AutoCamera>().Stop = true;
         fadeObjectOut.SetActive(true);
         GameManager.instance.Dying = true;
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(8);
 
         if (GameManager.instance.Lives > 0)
         {
